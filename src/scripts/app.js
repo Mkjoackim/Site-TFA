6599b00dcaa309f6578d07a6222ec7f8d0a7c151
Joackim maka <joackim.maka@student.heaj.be>
2023-06-07 21:07:06 +0200

    gsap

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,19 +1,13 @@
-/* user defined variables */
-var timeOnSlide = 3, 		
-    // the time each image will remain static on the screen, measured in seconds
-timeBetweenSlides = 1, 	
-    // the time taken to transition between images, measured in seconds
 
-// test if the browser supports animation, and if it needs a vendor prefix to do so
+var timeOnSlide = 3, 		
+timeBetweenSlides = 1, 	
     animationstring = 'animation',
     animation = false,
     keyframeprefix = '',
     domPrefixes = 'Webkit Moz O Khtml'.split(' '), 
-    // array of possible vendor prefixes
     pfx  = '',
     slidy = document.getElementById("slidy"); 
 if (slidy.style.animationName !== undefined) { animation = true; } 
-// browser supports keyframe animation w/o prefixes
 
 if( animation === false ) {
   for( var i = 0; i < domPrefixes.length; i++ ) {
@@ -32,31 +26,50 @@ if( animation === false ) {
   var images = slidy.getElementsByTagName("img"),
       firstImg = images[0], 
       imgWrap = firstImg.cloneNode(false); 
-  slidy.appendChild(imgWrap); // add the clone to the end of the images
-  var imgCount = images.length, // count the number of images in the slide, including the new cloned element
-      totalTime = (timeOnSlide + timeBetweenSlides) * (imgCount - 1), // calculate the total length of the animation by multiplying the number of _actual_ images by the amount of time for both static display of each image and motion between them
-      slideRatio = (timeOnSlide / totalTime)*100, // determine the percentage of time an induvidual image is held static during the animation
-      moveRatio = (timeBetweenSlides / totalTime)*100, // determine the percentage of time for an individual movement
-      basePercentage = 100/imgCount, // work out how wide each image should be in the slidy, as a percentage.
-      position = 0, // set the initial position of the slidy element
-      css = document.createElement("style"); // start marking a new style sheet
+  slidy.appendChild(imgWrap); 
+  var imgCount = images.length, 
+      totalTime = (timeOnSlide + timeBetweenSlides) * (imgCount - 1),
+      slideRatio = (timeOnSlide / totalTime)*100, 
+      moveRatio = (timeBetweenSlides / totalTime)*100, 
+      basePercentage = 100/imgCount, 
+      position = 0, 
+      css = document.createElement("style"); 
   css.type = "text/css";
   css.innerHTML += "#slidy { text-align: left; margin: 0; font-size: 0; position: relative; width: " + (imgCount * 100) + "%;  }\n"; // set the width for the slidy container
   css.innerHTML += "#slidy img { float: left; width: " + basePercentage + "%; }\n";
   css.innerHTML += "@"+keyframeprefix+"keyframes slidy {\n"; 
-  for (i=0;i<(imgCount-1); i++) { // 
-    position+= slideRatio; // make the keyframe the position of the image
+  for (i=0;i<(imgCount-1); i++) { 
+    position+= slideRatio; 
     css.innerHTML += position+"% { left: -"+(i * 100)+"%; }\n";
-    position += moveRatio; // make the postion for the _next_ slide
+    position += moveRatio; 
     css.innerHTML += position+"% { left: -"+((i+1) * 100)+"%; }\n";
 }
   css.innerHTML += "}\n";
   css.innerHTML += "#slidy { left: 0%; "+keyframeprefix+"transform: translate3d(0,0,0); "+keyframeprefix+"animation: "+totalTime+"s slidy infinite; }\n"; // call on the completed keyframe animation sequence
-document.body.appendChild(css); // add the new stylesheet to the end of the document
+document.body.appendChild(css);
 }
 
 
-const sr = ScrollReveal({
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".hero0", {
+    scrollTrigger:{
+        trigger: ".hero0", 
+        start: "top bottom", 
+        end: "bottom top", 
+        scrub: 1,
+        
+    },
+    x: -80,
+}) 
+
+
+
+
+
+
+/* const sr = ScrollReveal({
   origin: 'top',
   distance: '60px',
   duration: 2200,
@@ -67,6 +80,18 @@ sr.reveal(`.hero_unboring`)
  sr.reveal(`.hero_intro_text, .objectif, .obj, .pics, .ren, .card5, .subfooter__line`, {origin: 'bottom'})
 sr.reveal(`.asso, .hero_inter`, {origin: 'top'})
 sr.reveal(`sub__line, .exp__data, .exp__avis, .aside__grid, .hero_inter1`, {origin: 'left'})
-sr.reveal(`.obj3, .obj2`, {origin: 'right'})
+sr.reveal(`.obj3, .obj2`, {origin: 'right'}) */
  
 
+
+/* gsap.to(".image", {
+    scrollTrigger:{
+        trigger: ".image",
+        start: "top bottom", 
+        end: "bottom top", 
+        scrub: 1,
+        markers: true
+    },
+    x: -250, 
+
+})*/
